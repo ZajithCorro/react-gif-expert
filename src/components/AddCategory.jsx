3c2e80302264243f90aa62825f716d2dfb39679b
Zajith Corro <zaja_9508@hotmail.com>
2020-07-22 15:29:27 -0500

    Creating AddCategory component
    This component has the ability to add a new category

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function AddCategory({ setCategories }) {
+	const [value, setValue] = useState('');
+
+	const handleInputChange = (e) => {
+		setValue(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (value.trim().length > 2) {
+			setCategories((categories) => [...categories, value]);
+			setValue('');
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type='text' value={value} onChange={handleInputChange} />
+		</form>
+	);
+}
+
+AddCategory.propTypes = {
+	setCategories: PropTypes.func.isRequired,
+};
