adafdbba5423e907b3018ccba76c7be18fdacdb4
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-07-22 21:53:31 -0500

    Changing the order to push the data

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,28 +2,28 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function AddCategory({ setCategories }) {
-	const [value, setValue] = useState('');
+  const [value, setValue] = useState('');
 
-	const handleInputChange = (e) => {
-		setValue(e.target.value);
-	};
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		if (value.trim().length > 2) {
-			setCategories((categories) => [...categories, value]);
-			setValue('');
-		}
-	};
+    if (value.trim().length > 2) {
+      setCategories((categories) => [value, ...categories]);
+      setValue('');
+    }
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input type='text' value={value} onChange={handleInputChange} />
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={value} onChange={handleInputChange} />
+    </form>
+  );
 }
 
 AddCategory.propTypes = {
-	setCategories: PropTypes.func.isRequired,
+  setCategories: PropTypes.func.isRequired,
 };
