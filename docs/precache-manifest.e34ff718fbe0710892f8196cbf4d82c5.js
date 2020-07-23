0f346ad94af4d0bcd4f144ac4a445bdf5dd10942
Zajith Corro Viveros <zaja_9508@hotmail.com>
2020-07-22 23:03:10 -0500

    Deploying app in github pages

---
+++ docs/precache-manifest.e34ff718fbe0710892f8196cbf4d82c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2039f6130193f8505c219e3dd270512e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7bac58a6415d3ca7ea",
+    "url": "/static/css/main.c9658397.chunk.css"
+  },
+  {
+    "revision": "eb00bc0e5d1d87ed4120",
+    "url": "/static/js/2.34972069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34972069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7bac58a6415d3ca7ea",
+    "url": "/static/js/main.3c004a32.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
